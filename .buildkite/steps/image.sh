3018facd2aeff4cbf036ab9ad202d5db95e587c2
jmcshane <james.mcshane@superorbital.io>
2023-01-20 21:38:19 -0500

    Starting build

---
+++ .buildkite/steps/image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+apk add jq
+
+mkdir ~/.docker
+cp /creds/.docker/config.json ~/.docker/config.json
+
+docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
+
+docker buildx use remote
+docker buildx build --tag="ghcr.io/superorbital/rails-docker-parallel-example:${BUILDKITE_COMMIT}" --platform=linux/arm64 --push --metadata-file="$(pwd)/metadata.json" --build-arg REPO="${BUILDKITE_REPO}" .
+
+image=$(jq -r '"\(."image.name")@\(."containerimage.digest")"' metadata.json)
+buildkite-agent meta-data set "image" "${image}"
