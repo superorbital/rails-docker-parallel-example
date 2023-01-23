6713ed836835324ba9a8016b5a10348b33d7f787
jmcshane <james.mcshane@superorbital.io>
2023-01-23 11:01:30 -0500

    Envsubst image issue

--- .buildkite/steps/manifests.sh
+++ .buildkite/steps/manifests.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-set -euxo pipefail
+set -e
 
 export BUILDKITE_K8S_JOB_UID
 BUILDKITE_K8S_JOB_UID=$(kubectl get job -n "${BUILDKITE_NAMESPACE}" "${BUILDKITE_K8S_JOB_NAME}" -o jsonpath="{.metadata.uid}")
