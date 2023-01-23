45f70e9f7151efafaf03618eb5dacd3ad2279a64
jmcshane <james.mcshane@superorbital.io>
2023-01-23 12:31:41 -0500

    Some sleep

--- .buildkite/steps/apply.sh
+++ .buildkite/steps/apply.sh
@@ -8,4 +8,6 @@ helm template ./.buildkite/steps/db-chart \
   --set buildkite.pod=${BUILDKITE_K8S_POD} \
   --set buildkite.job=${BUILDKITE_K8S_JOB_NAME} | kubectl apply -f -
 
+sleep 180
+
 kubectl wait pods -n ${BUILDKITE_NAMESPACE} -l job=buildkite-${BUILDKITE_JOB_ID} --for condition=Ready --timeout=60s
