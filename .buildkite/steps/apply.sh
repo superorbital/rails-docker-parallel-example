b5bd4cf30ebba1cd957e12a8c6ee6fbac09ad059
jmcshane <james.mcshane@superorbital.io>
2023-01-24 14:32:57 -0500

    Single pipeline

--- .buildkite/steps/apply.sh
+++ .buildkite/steps/apply.sh
@@ -6,8 +6,8 @@ helm template ./.buildkite/steps/db-chart \
   --set buildkite.namespace=${BUILDKITE_NAMESPACE} \
   --set buildkite.pod_uid=${BUILDKITE_K8S_POD_UID} \
   --set buildkite.pod=${BUILDKITE_K8S_POD} \
-  --set buildkite.job=${BUILDKITE_K8S_JOB_NAME} | kubectl apply -f -
+  --set buildkite.job=${BUILDKITE_JOB_ID} | kubectl apply -f -
 
-sleep 180
+sleep 5
 
-kubectl wait pods -n ${BUILDKITE_NAMESPACE} -l job=buildkite-${BUILDKITE_JOB_ID} --for condition=Ready --timeout=60s
+kubectl wait pods -n ${BUILDKITE_NAMESPACE} -l job=${BUILDKITE_JOB_ID} --for condition=Ready --timeout=60s
--- .buildkite/steps/parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-export BUILT_IMAGE
-
-BUILT_IMAGE=$(buildkite-agent meta-data get "image")
-
-buildkite-agent pipeline upload .buildkite/parallel.yml
