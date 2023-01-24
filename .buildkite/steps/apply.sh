47099fc111d4fe09850f59f5711c150e2791b8ab
jmcshane <james.mcshane@superorbital.io>
2023-01-24 14:44:34 -0500

    Wait

--- .buildkite/steps/apply.sh
+++ .buildkite/steps/apply.sh
@@ -6,8 +6,8 @@ helm template ./.buildkite/steps/db-chart \
   --set buildkite.namespace=${BUILDKITE_NAMESPACE} \
   --set buildkite.pod_uid=${BUILDKITE_K8S_POD_UID} \
   --set buildkite.pod=${BUILDKITE_K8S_POD} \
-  --set buildkite.job=${BUILDKITE_JOB_ID} | kubectl apply -f -
+  --set buildkite.job=${BUILDKITE_JOB_ID} | kubectl apply --wait=true -f -
 
-sleep 5
+sleep 10
 
 kubectl wait pods -n ${BUILDKITE_NAMESPACE} -l job=${BUILDKITE_JOB_ID} --for condition=Ready --timeout=60s
